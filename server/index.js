b77f44e3622a8998d6c1ae8a853ee749f8394df4
Ushitora Anqou <ushitora_anqou@yahoo.co.jp>
2019-02-06 23:25:06 +0900

    Build mock-up for quiz

--- server/index.js
+++ server/index.js
@@ -8,18 +8,64 @@ const uuid = require('uuid/v4')
 
 console.log('start listening at port ' + port)
 
+db = {
+  room: {},
+  user: {},
+  socket: {}
+}
+
+const roomid = 'default room'
+db.room[roomid] = {
+  stage: 0,
+  master: null
+}
+
 io.on('connection', socket => {
   const log = msg => {
     console.log('[' + socket.id + '] ' + msg)
   }
 
-  log('Join')
+  const uid = uuid()
+  const room = db.room[roomid]
+  db.user[uid] = { sid: socket.id }
+  db.socket[socket.id] = { uid: uid }
 
-  socket.join('default room')
+  log('Join in ' + roomid)
+
+  socket.join(roomid)
 
   socket.on('chat-msg', msg => {
     log('chat-msg: ' + msg)
-    io.emit('chat-msg', { id: uuid(), body: msg })
+    io.to(roomid).emit('chat-msg', { id: uuid(), body: msg })
+  })
+
+  socket.on('quiz-music', msg => {
+    //if (room.stage != 0) {
+    //  log('invalid stage')
+    //  return
+    //}
+
+    log('quiz-music: ' + msg.buf.length)
+
+    room.stage = 1
+    room.master = uid
+
+    socket.broadcast.to(roomid).emit('quiz-music', msg)
+  })
+
+  socket.on('quiz-answer', msg => {
+    log('quiz-answer: ' + msg.answer)
+
+    io.to(db.user[room.master].sid).emit('quiz-answer', {
+      uid: uid,
+      answer: msg.answer
+    })
+  })
+
+  socket.on('quiz-result', msg => {
+    log('quiz-result: ' + msg)
+
+    socket.broadcast.to(roomid).emit('quiz-result', msg)
   })
 
   socket.on('disconnect', () => {
--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import update from 'immutability-helper'
 import './App.css'
 import { config } from './config'
 
+// thanks to https://simon-schraeder.de/posts/filereader-async/
+function readFileAsync (file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader()
+
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+
+    reader.onerror = reject
+
+    reader.readAsArrayBuffer(file)
+  })
+}
+
+const socket = io(config.server_uri)
+
 class ChatWindow extends Component {
   constructor (props) {
     super(props)
@@ -12,8 +29,6 @@ class ChatWindow extends Component {
       history: [],
       input: ''
     }
-
-    this.socket = props.socket
   }
 
   onChatMsg = msg => {
@@ -23,16 +38,16 @@ class ChatWindow extends Component {
   }
 
   componentDidMount () {
-    this.socket.on('chat-msg', this.onChatMsg)
+    socket.on('chat-msg', this.onChatMsg)
   }
 
   componentWillUnmount () {
-    this.socket.off('chat-msg', this.onChatMsg)
+    socket.off('chat-msg', this.onChatMsg)
   }
 
   onSend = () => {
     if (this.state.input !== '') {
-      this.socket.emit('chat-msg', this.state.input)
+      socket.emit('chat-msg', this.state.input)
       this.setState({ input: '' })
     }
   }
@@ -63,23 +78,205 @@ class ChatWindow extends Component {
   }
 }
 
-class App extends Component {
+function changeScene (self, SceneComponent, props) {
+  self.props.changeScene(
+    <SceneComponent changeScene={self.props.changeScene} {...props} />
+  )
+}
+
+class WaitMusic extends Component {
+  constructor (props) {
+    super(props)
+
+    this.inputMusicFile = React.createRef()
+  }
+
+  onQuizMusic = msg => {
+    changeScene(this, PlayMusic, { music: msg.buf })
+  }
+
+  componentDidMount () {
+    socket.on('quiz-music', this.onQuizMusic)
+  }
+
+  componentWillUnmount () {
+    socket.off('quiz-music', this.onQuizMusic)
+  }
+
+  onClickSendMusic = () => {
+    const file = this.inputMusicFile.current.files[0]
+
+    readFileAsync(file).then(buf => {
+      socket.emit('quiz-music', { buf: buf })
+    })
+
+    changeScene(this, ShowResult, { judge: true })
+  }
+
+  render () {
+    return (
+      <div className='WaitMusic'>
+        <input type='file' ref={this.inputMusicFile} />
+        <button onClick={this.onClickSendMusic}>Send</button>
+        <p>Waiting music</p>
+      </div>
+    )
+  }
+}
+
+class PlayMusic extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      chat: { dummy: 'ABC', history: [] }
+      playing: false
     }
+    this.music = { buf: props.music }
+  }
 
-    this.socket = io(config.server_uri)
+  onClickStart = () => {
+    this.setState({ playing: true })
+  }
+
+  onClickStop = () => {
+    changeScene(this, InputAnswer, {})
+  }
+
+  render () {
+    return (
+      <div className='PlayMusic'>
+        {this.state.playing ? (
+          <button onClick={this.onClickStop}>Stop</button>
+        ) : (
+          <button onClick={this.onClickStart}>Start</button>
+        )}
+      </div>
+    )
+  }
+}
+
+class InputAnswer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.inputAnswer = React.createRef()
+  }
+
+  onSend = () => {
+    socket.emit('quiz-answer', { answer: this.inputAnswer.current.value })
+    changeScene(this, ShowResult, { judge: false })
+  }
+
+  render () {
+    return (
+      <div className='InputAnswer'>
+        <input type='text' ref={this.inputAnswer} />
+        <button onClick={this.onSend}>Send</button>
+      </div>
+    )
+  }
+}
+
+class ShowResult extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      judging: props.judge,
+      entries: []
+    }
+  }
+
+  componentDidMount () {
+    socket.on('quiz-answer', this.onQuizAnswer)
+    socket.on('quiz-result', this.onQuizResult)
+  }
+
+  componentWillUnmount () {
+    socket.off('quiz-answer', this.onQuizAnswer)
+    socket.off('quiz-result', this.onQuizResult)
+  }
+
+  onQuizAnswer = msg => {
+    this.setState((state, props) => {
+      return update(state, { entries: { $push: [msg] } })
+    })
+  }
+
+  onQuizResult = msg => {
+    this.setState({ entries: msg })
+  }
+
+  onSendJudge = () => {
+    socket.emit('quiz-result', this.state.entries)
+    this.setState({ judging: false })
+  }
+
+  onSendDone = () => {
+    changeScene(this, WaitMusic, {})
+  }
+
+  render () {
+    const entries = this.state.entries
+    return (
+      <div className='ShowResult'>
+        {entries.length === 0 ? (
+          <p>Waiting for the result</p>
+        ) : (
+          <div>
+            <table>
+              <tbody>
+                {entries.map(entry => (
+                  <tr key={entry.uid}>
+                    <td>{entry.uid}</td>
+                    <td>{entry.answer}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            {this.state.judging && (
+              <button onClick={this.onSendJudge}>Send</button>
+            )}
+            {!this.state.judging && (
+              <button onClick={this.onSendDone}>Done</button>
+            )}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+class SceneView extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      scene: <WaitMusic changeScene={this.changeScene} />
+    }
+  }
+
+  changeScene = scene => {
+    this.setState({ scene: scene })
+  }
+
+  render () {
+    return <div className='SceneView'>{this.state.scene}</div>
+  }
+}
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
   }
 
   render () {
     return (
       <div className='App'>
         <h1>Hello holo</h1>
-        <p>Waiting music</p>
-        <ChatWindow socket={this.socket} />
+        <SceneView />
+        <ChatWindow />
       </div>
     )
   }
