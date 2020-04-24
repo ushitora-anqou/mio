944007af3ad24f9dfa4ae2ebf08f95a47b0e2f72
hiromi-mi <hiromi-mi@cat.zaq.jp>
2020-04-24 18:50:45 +0900

    works

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ const schema = {}
 // 104
 // # Therefore the length of 128 is enough :)
 schema.name = Joi.string().max(128)
-schema.roomid = Joi.string().uuid('uuidv4')
-schema.uid = Joi.string().uuid('uuidv4')
-schema.password = Joi.string().uuid('uuidv4')
+// uuid() function is obsolete; use guid()
+schema.roomid = Joi.string().guid({version: ['uuidv4']})
+schema.uid = Joi.string().guid({version: ['uuidv4']})
+schema.password = Joi.string().guid({version: ['uuidv4']})
 schema.time = Joi.number().min(0)
 schema.allotment = Joi.number()
   .integer()
